915438d441def6716c04249d31fb25f74bfbc9c0
hassangamal <gamalhassan807@yahoo.com>
2018-04-26 18:15:33 +0200

    Angular_WebAPI

---
+++ WepAPI/QuizAPI/QuizAPI/Controllers/ParicipantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using QuizAPI.Models;
+namespace QuizAPI.Controllers
+{
+    public class ParicipantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+        public Participant Insert( Participant model)
+        {
+            using(DBModel db=new DBModel())
+            {
+                db.Participants.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/UpdateParticipant")]
+        public void Update(Participant model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Entry(model).State=System.Data.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+        }
+
+    }
+}
